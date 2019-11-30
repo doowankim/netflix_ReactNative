9483b4d5ba513a124fcd9f76b584027d29ed6c34
Doowankim <rlaendhks11@gmail.com>
2019-11-30 11:37:44 +0900

    preLoading assets

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { AppLoading } from "expo";
+import * as Font from 'expo-font';
+import { Ionicons } from "@expo/vector-icons";
 import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={{flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends Component {
+    state = {
+        loaded: false
+    };
+
+    handleError = error => console.log(error);
+
+    handleLoaded = () => this.setState({ loaded: true });
+
+    loadAssets = async () => {
+        await Font.loadAsync({
+            ...Ionicons.font //전체내용에 폰트를 적용
+        });
+    };
+    render() {
+        const {loaded} = this.state;
+        if(loaded) {
+            return (
+                <View>
+                    <Text>asdklfjalsdkjf</Text>
+                </View>
+            );
+        } else {
+            return (
+                <AppLoading
+                    startAsync={this.loadAssets}
+                    onFinish={this.handleLoaded}
+                    onError={this.handleError}
+                />
+            );
+        }
+    }
 }
+
+export default App;
